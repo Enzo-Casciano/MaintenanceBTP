ba3b3601ddfcbcfb889c7861be4eb7e72f78e909
Antoine72210 <antoine.bouchet72@outlook.fr>
2020-12-03 19:43:43 +0100

    feature: Gestion d'une demande resolue

--- src/Controller/InterventionController.php
+++ src/Controller/InterventionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Criticite;
 use App\Entity\Intervention;
 use App\Entity\Materiel;
 use App\Entity\Niveau;
@@ -67,10 +68,14 @@ class InterventionController extends AbstractController
                          ->getRepository(Materiel::class)
                          ->find($id);
 
+        $criticite = $this->getDoctrine()
+                         ->getRepository(Criticite::class)
+                         ->find($id);
         $changementStatutEnCours = $statutRep->updateStatutTicket($id, 5);
 
     return $this->render('intervention/index.html.twig',[
          'form' => $form->createView(),
+         'criticite' => $criticite,
          'ticket' => $ticket,
          'utilisateur' => $utilisateur,
          'salle' => $salle,
